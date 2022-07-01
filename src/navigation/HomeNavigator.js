31c367c26ccaa7dba63084f8f4b902e3c75ccd42
Nhat Minh <minh.nhat.nguyen@mediastep.com>
2022-07-01 08:54:17 +0700

    First commit

---
+++ src/navigation/HomeNavigator.js
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+//
+import { NAVIGATION } from '@/constants';
+import { Home } from '@/screens';
+
+const Stack = createNativeStackNavigator();
+
+export function HomeNavigator() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name={NAVIGATION.home} component={Home} />
+    </Stack.Navigator>
+  );
+}
