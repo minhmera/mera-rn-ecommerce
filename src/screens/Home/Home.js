778a62928932ea1b977a7c065e9f7ff45493cabb
Nhat Minh <minh.nhat.nguyen@mediastep.com>
2022-07-01 14:27:10 +0700

    restructer css

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -53,64 +53,15 @@ export function Home({ navigation, route }) {
           marginVertical: 14,
         }}
       >
-        <View
-          style={{
-            width: '100%',
-            height: 100,
-            borderRadius: 10,
-            backgroundColor: COLOURS.backgroundLight,
-            position: 'relative',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginBottom: 8,
-          }}
-        >
+        <View style={styles.itemDetail}>
           {data.isOff ? (
-            <View
-              style={{
-                position: 'absolute',
-                width: '20%',
-                height: '24%',
-                backgroundColor: COLOURS.green,
-                top: 0,
-                left: 0,
-                borderTopLeftRadius: 10,
-                borderBottomRightRadius: 10,
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 12,
-                  color: COLOURS.white,
-                  fontWeight: 'bold',
-                  letterSpacing: 1,
-                }}
-              >
-                {data.offPercentage}%
-              </Text>
+            <View style={styles.discountView}>
+              <Text style={styles.discountText}>{data.offPercentage}%</Text>
             </View>
           ) : null}
-          <Image
-            source={data.productImage}
-            style={{
-              width: '80%',
-              height: '80%',
-              resizeMode: 'contain',
-            }}
-          />
+          <Image source={data.productImage} style={styles.productImage} />
         </View>
-        <Text
-          style={{
-            fontSize: 12,
-            color: COLOURS.black,
-            fontWeight: '600',
-            marginBottom: 2,
-          }}
-        >
-          {data.productName}
-        </Text>
+        <Text style={styles.productName}>{data.productName}</Text>
         {data.category == 'accessory' ? (
           data.isAvailable ? (
             <View
@@ -121,44 +72,17 @@ export function Home({ navigation, route }) {
             >
               <FontAwesome
                 name="circle"
-                style={{
-                  fontSize: 12,
-                  marginRight: 6,
-                  color: COLOURS.green,
-                }}
+                style={{ fontSize: 12, marginRight: 6, color: COLOURS.green }}
               />
-              <Text
-                style={{
-                  fontSize: 12,
-                  color: COLOURS.green,
-                }}
-              >
-                Available
-              </Text>
+              <Text style={{ fontSize: 12, color: COLOURS.green }}>Available</Text>
             </View>
           ) : (
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}
-            >
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
               <FontAwesome
                 name="circle"
-                style={{
-                  fontSize: 12,
-                  marginRight: 6,
-                  color: COLOURS.red,
-                }}
+                style={{ fontSize: 12, marginRight: 6, color: COLOURS.red }}
               />
-              <Text
-                style={{
-                  fontSize: 12,
-                  color: COLOURS.red,
-                }}
-              >
-                Unavailable
-              </Text>
+              <Text style={{ fontSize: 12, color: COLOURS.red }}>Unavailable</Text>
             </View>
           )
         ) : null}
@@ -166,24 +90,9 @@ export function Home({ navigation, route }) {
       </TouchableOpacity>
     );
   };
-  // return (
-  //   <View style={styles.container}>
-  //     <Text style={[typography.title, { color: colors.text }]}>
-  //       {strings.home.message} {user?.username}
-  //     </Text>
-  //     <Text style={[typography.text, { color: colors.text }]}>
-  //       {strings.home.variant} {Config.BUILD_VARIANT}
-  //     </Text>
-  //   </View>
-  // );
+
   return (
-    <View
-      style={{
-        width: '100%',
-        height: '100%',
-        backgroundColor: COLOURS.white,
-      }}
-    >
+    <View style={styles.container}>
       <StatusBar backgroundColor={COLOURS.white} barStyle="dark-content" />
       <ScrollView showsVerticalScrollIndicator={false}>
         <View
@@ -223,93 +132,24 @@ export function Home({ navigation, route }) {
             />
           </TouchableOpacity>
         </View>
-        <View
-          style={{
-            marginBottom: 10,
-            padding: 16,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 26,
-              color: COLOURS.black,
-              fontWeight: '500',
-              letterSpacing: 1,
-              marginBottom: 10,
-            }}
-          >
-            Hi-Fi Shop &amp; Service
-          </Text>
-          <Text
-            style={{
-              fontSize: 14,
-              color: COLOURS.black,
-              fontWeight: '400',
-              letterSpacing: 1,
-              lineHeight: 24,
-            }}
-          >
+        <View style={{ marginBottom: 10, padding: 16 }}>
+          <Text style={styles.titleText}>Hi-Fi Shop & Service</Text>
+          <Text style={styles.titleText2}>
             Audio shop on Rustaveli Ave 57.
             {'\n'}This shop offers both products and services
           </Text>
         </View>
-        <View
-          style={{
-            padding: 16,
-          }}
-        >
+        <View style={{ padding: 16 }}>
           <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-            }}
+            style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}
           >
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 18,
-                  color: COLOURS.black,
-                  fontWeight: '500',
-                  letterSpacing: 1,
-                }}
-              >
-                Products
-              </Text>
-              <Text
-                style={{
-                  fontSize: 14,
-                  color: COLOURS.black,
-                  fontWeight: '400',
-                  opacity: 0.5,
-                  marginLeft: 10,
-                }}
-              >
-                41
-              </Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text style={styles.categoryText}>Products</Text>
+              <Text style={styles.categoryNumber}>41</Text>
             </View>
-            <Text
-              style={{
-                fontSize: 14,
-                color: COLOURS.blue,
-                fontWeight: '400',
-              }}
-            >
-              SeeAll
-            </Text>
+            <Text style={styles.seeAllText}>SeeAll</Text>
           </View>
-          <View
-            style={{
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-              justifyContent: 'space-around',
-            }}
-          >
+          <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around' }}>
             {products.map((data) => {
               return <ProductCard data={data} key={data.id} />;
             })}
@@ -322,57 +162,15 @@ export function Home({ navigation, route }) {
           }}
         >
           <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-            }}
+            style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}
           >
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 18,
-                  color: COLOURS.black,
-                  fontWeight: '500',
-                  letterSpacing: 1,
-                }}
-              >
-                Accessories
-              </Text>
-              <Text
-                style={{
-                  fontSize: 14,
-                  color: COLOURS.black,
-                  fontWeight: '400',
-                  opacity: 0.5,
-                  marginLeft: 10,
-                }}
-              >
-                78
-              </Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text style={styles.categoryText}>Accessories</Text>
+              <Text style={styles.categoryNumber}>78</Text>
             </View>
-            <Text
-              style={{
-                fontSize: 14,
-                color: COLOURS.blue,
-                fontWeight: '400',
-              }}
-            >
-              SeeAll
-            </Text>
+            <Text style={styles.seeAllText}>SeeAll</Text>
           </View>
-          <View
-            style={{
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-              justifyContent: 'space-around',
-            }}
-          >
+          <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around' }}>
             {accessory.map((data) => {
               return <ProductCard data={data} key={data.id} />;
             })}
@@ -382,3 +180,14 @@ export function Home({ navigation, route }) {
     </View>
   );
 }
+
+// return (
+//     <View style={styles.container}>
+//         <Text style={[typography.title, { color: colors.text }]}>
+//             {strings.home.message} {user?.username}
+//         </Text>
+//         <Text style={[typography.text, { color: colors.text }]}>
+//             {strings.home.variant} {Config.BUILD_VARIANT}
+//         </Text>
+//     </View>
+// );
--- src/screens/Home/Home.styles.js
+++ src/screens/Home/Home.styles.js
@@ -1,9 +1,82 @@
 import { StyleSheet } from 'react-native';
+import { COLOURS, Items } from '../../mocks/database/Database';
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '100%',
+    height: '100%',
+    backgroundColor: COLOURS.white,
+  },
+  titleText: {
+    fontSize: 26,
+    color: COLOURS.black,
+    fontWeight: '500',
+    letterSpacing: 1,
+    marginBottom: 10,
+  },
+  titleText2: {
+    fontSize: 14,
+    color: COLOURS.black,
+    fontWeight: '400',
+    letterSpacing: 1,
+    lineHeight: 24,
+  },
+  categoryText: {
+    fontSize: 18,
+    color: COLOURS.black,
+    fontWeight: '500',
+    letterSpacing: 1,
+  },
+  categoryNumber: {
+    fontSize: 14,
+    color: COLOURS.black,
+    fontWeight: '400',
+    opacity: 0.5,
+    marginLeft: 10,
+  },
+  seeAllText: {
+    fontSize: 14,
+    color: COLOURS.blue,
+    fontWeight: '400',
+  },
+
+  itemDetail: {
+    width: '100%',
+    height: 100,
+    borderRadius: 10,
+    backgroundColor: COLOURS.backgroundLight,
+    position: 'relative',
     justifyContent: 'center',
     alignItems: 'center',
+    marginBottom: 8,
+  },
+  discountView: {
+    position: 'absolute',
+    width: '20%',
+    height: '24%',
+    backgroundColor: COLOURS.green,
+    top: 0,
+    left: 0,
+    borderTopLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  discountText: {
+    fontSize: 12,
+    color: COLOURS.white,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+  },
+  productImage: {
+    width: '80%',
+    height: '80%',
+    resizeMode: 'contain',
+  },
+  productName: {
+    fontSize: 12,
+    color: COLOURS.black,
+    fontWeight: '600',
+    marginBottom: 2,
   },
 });
